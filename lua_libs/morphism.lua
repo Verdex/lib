0cabf086113936a647fe682629d590597f6b8c30
David L. Inman <davidlevere@gmail.com>
2013-05-09 20:30:30 -0400

    implemented hylomorphism

--- lua_libs/morphism.lua
+++ lua_libs/morphism.lua
@@ -2,21 +2,21 @@
 module( ..., package.seeall )
 
 
-local function anaHelper( accum, gen, pred, b )
-    if pred( b ) then
+local function anaHelper( accum, gen, pred, s )
+    if pred( s ) then
         return accum
     end
 
-    local a, b = gen( b )
+    local a, s = gen( s )
     accum[#accum + 1] = a
-    return anaHelper( accum, gen, pred, b )
+    return anaHelper( accum, gen, pred, s )
 end
 
 -- generator : b -> (a, b)
 -- stopPred : b -> bool
 -- anamorphism : b -> [a]
 function ana( generator, stopPred )
-    return function( b ) return anaHelper( {}, generator, stopPred, b )
+    return function( seed ) return anaHelper( {}, generator, stopPred, seed )
 end
 
 -- initial : b
@@ -29,4 +29,18 @@ function cata( initial, folder )
         end
         return initial
     end
+end
+
+-- TODO make sure to test this, im not convinced i got this right
+local function hyloHelper( i, fold, gen, pred, s )
+    if pred( s ) then
+        return i 
+    end
+   
+    local a, s = gen( s )
+    return fold( a, hyloHelper( i, folder, gen, pred, s ) )
+end 
+    
+function hylo( initial, folder, generator, stopPred )
+    return function( seed ) return hyloHelper( initial, folder, generator, stopPred, seed )
 end
