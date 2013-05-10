a2674c54c2a0ee1f6ddd9e5e680b48ca453545f1
David L. Inman <davidlevere@gmail.com>
2013-05-09 20:11:57 -0400

    added anamorphism

--- lua_libs/morphism.lua
+++ lua_libs/morphism.lua
@@ -2,3 +2,19 @@
 module( ..., package.seeall )
 
 
+local function anaHelper( accum, gen, pred, b )
+    if pred( b ) then
+        return accum
+    end
+
+    local a, b = gen( b )
+    accum[#accum + 1] = a
+    return anaHelper( accum, gen, pred, b )
+end
+
+-- generator : b -> (a, b)
+-- stopPred : b -> bool
+-- anamorphism : b -> [a]
+function ana( generator, stopPred )
+    return function( b ) return anaHelper( {}, generator, stopPred, b )
+end
