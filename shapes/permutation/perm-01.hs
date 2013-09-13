564f438ecc0d6d787e38a675db7d3cb7b449e342
David L. Inman <davidlevere@gmail.com>
2013-09-12 23:35:15 -0400

    I seem to have made some sort of a mess
    going to try to find my way back

--- shapes/permutation/perm-01.hs
+++ shapes/permutation/perm-01.hs
@@ -6,6 +6,29 @@ ikky :: (a->b) -> (b->[a]->c) -> [a] -> [a] -> [c]
 ikky f g _ [] = []
 ikky f g as (b:bs) = (g (f b) (as ++ bs)) : ikky f g (as++[b]) bs
 
-perm [] = [] -- ?
-perm [a] = [a]
-perm as = undefined
+--jabber :: [a] -> [[a]]
+--jabber a = ikky 
+
+
+--perm [] = [[]] -- ?
+--perm as = ikky (\j-> (j:)) (\p-> \d -> map p (perm d)) [] as
+
+half ((a:as),bs)
+    | length (a:as) <= length bs = [a:as, bs]
+    | otherwise = half (as, a:bs)
+
+halfList as = half (as,[])
+
+mergeLists [[]] = []
+mergeLists 
+
+mergeList a [] = a
+mergeList [] b = b
+mergeList (a:as) (b:bs)
+    | a < b = a : mergeList as (b:bs)
+    | otherwise = b : mergeList (a:as) bs
+
+wocky _ _ [] = []
+wocky split combine as = combine (map (wocky split combine) (split as))
+
+mergeSort = wocky halfList mergeList
