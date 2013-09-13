844e74edb65c48d43ce81b4348586d3aa8326470
David L. Inman <davidlevere@gmail.com>
2013-09-12 22:10:03 -0400

    okay, ikky is the function that I want
    I can plug it into a generic "perm" function and I believe that
    I can swap it out to go from a sort to a permutation by using
    the same skeleton function

--- shapes/permutation/perm-01.hs
+++ shapes/permutation/perm-01.hs
@@ -1,10 +1,10 @@
 
--- initial permutation
-
-
 blah _ [] = []
 blah as (b1:bs) = (show b1,show (as ++ bs)) : blah (as++[b1]) bs
 
+ikky :: (a->b) -> (b->[a]->c) -> [a] -> [a] -> [c]
+ikky f g _ [] = []
+ikky f g as (b:bs) = (g (f b) (as ++ bs)) : ikky f g (as++[b]) bs
 
 perm [] = [] -- ?
 perm [a] = [a]
