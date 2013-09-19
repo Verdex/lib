e9cae5e7040d9e52f03aadddc769a97c7aebc8e1
David L. Inman <davidlevere@gmail.com>
2013-09-18 22:52:46 -0400

    I think I have a generic wocky that I like

--- shapes/permutation/perm-01.hs
+++ shapes/permutation/perm-01.hs
@@ -1,12 +1,4 @@
 
-blah _ [] = []
-blah as (b1:bs) = (show b1,show (as ++ bs)) : blah (as++[b1]) bs
-
-ikky :: (a->b) -> (b->[a]->c) -> [a] -> [a] -> [c]
-ikky f g _ [] = []
-ikky f g as (b:bs) = (g (f b) (as ++ bs)) : ikky f g (as++[b]) bs
-
-
 half ([],bs) = [[],bs]
 half ((a:as),bs)
     | length (a:as) <= length bs = [(a:as),bs]
@@ -23,15 +15,21 @@ mergeList (a:as) (b:bs)
     | a < b = a : mergeList as (b:bs)
     | otherwise = b : mergeList (a:as) bs
 
-wocky _ _ [] = []
-wocky _ _ [a] = [a]
-wocky split combine as = combine (map (wocky split combine) (split as))
+
+wocky _ s c [] = c (s [])
+wocky _ s c [a] = c (s [a])
+wocky trans split combine as = combine (map (trans (wocky trans split combine)) (split as))
 
 mergeSort :: Ord a => [a] -> [a]
-mergeSort = wocky halfList mergeLists
+mergeSort = wocky id halfList mergeLists
 
 
 --split : [a] -> [[a]]
 --comb : [[a]] -> [b]
 
---final :: [a] -> [b]
+-- map : ((c, [a]) -> (c, [a])) -> [(c, [a])]
+-- trans : ([a] -> [a]) -> (c, [a]) -> (c, [a]) 
+-- split : [a] -> [(c, [a])]
+-- comb : [(c, [a])] -> [a]
+-- wocky : [a] -> [b]
+
