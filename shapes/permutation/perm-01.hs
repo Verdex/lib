e246c7c05fbf55e93014610035f3212d45056406
David L. Inman <davidlevere@gmail.com>
2013-09-19 20:50:01 -0400

    Not sure I'm making progress or not.
    new permutation intermediate function

--- shapes/permutation/perm-01.hs
+++ shapes/permutation/perm-01.hs
@@ -23,12 +23,12 @@ mergeList (a:as) (b:bs)
     | otherwise = b : mergeList (a:as) bs
 
 --wocky :: (([a] -> b) -> c -> c) -> ([a] -> [c]) -> ([c] -> b) -> [a] -> b
-wocky b t s c [] = c (map (t (\ as -> b)) (s []))
+--wocky b t s c [] = c (map (t (\ as -> b)) (s []))
 --wocky b t s c [a] = c (map (t (wocky t s c)) (s [a]))
-wocky b trans split combine as = combine (map (trans (wocky trans split combine)) (split as))
+--wocky b trans split combine as = combine (map (trans (wocky trans split combine)) (split as))
 
-mergeSort :: Ord a => [a] -> [a]
-mergeSort = wocky [] id halfList mergeLists
+--mergeSort :: Ord a => [a] -> [a]
+--mergeSort = wocky [] id halfList mergeLists
 
 permSplit :: [a] -> [(a, [a])]
 permSplit = permSplitHelper []
@@ -41,9 +41,21 @@ permComb = foldl (++) []
 permTrans :: ([a] -> [[a]]) -> (a, [a]) -> [[a]] 
 permTrans f (c, as) = map (c:) (f as)
 
-perm :: [a] -> [[a]]
-perm = wocky [[]] permTrans permSplit permComb
+--perm :: [a] -> [[a]]
+--perm = wocky [[]] permTrans permSplit permComb
 
 perm2 :: [a] -> [[a]]
 perm2 [] = [[]]
 perm2 as = foldl (++) [] (map ( \ (b, bs) -> map (b:) (perm2 bs) ) (permSplit as))
+
+perm3 :: ((f a -> j a) -> m (f a) -> j a) 
+            -> ([j a] -> j a) 
+            -> (f a -> [m (f a)])
+            -> f a -> j a
+
+perm3 t c s as = c (map (t (perm3 t c s)) (s as))
+
+--perm4 :: [a] -> [[a]]
+--perm4 = perm3 permTrans permComb permSplit
+
+
